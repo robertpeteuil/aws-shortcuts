59b5bc36446361f3a798caade1c077c4bc0ea8e5
Robert Peteuil <robert@peteuil.com>
2017-04-24 08:29:54 -0700

    Changed Process for Getting Instance Tag Value
    created separate function in awsc with instance-id and optional ‘tag-name’ as inputs, returns tag value
    	adds ability to use the function independently for other tags in future
    	prevents error if instance has no tags
    awsc.getdetails calls this new funct to populate tag value in instance-data dictionary
    	this reduces complexity and CC

--- awss/awsc.py
+++ awss/awsc.py
@@ -17,8 +17,8 @@ def getids(QueryString=None):
     instanceSummaryData = eval(QueryString)
     iInfo = {}
     for i, v in enumerate(instanceSummaryData['Reservations']):
-        inID = v['Instances'][0]['InstanceId']
-        iInfo[i] = {'id': inID}
+        instID = v['Instances'][0]['InstanceId']
+        iInfo[i] = {'id': instID}
     debg.dprint("numInstances: ", len(iInfo))
     debg.dprintx("InstanceIds Only")
     debg.dprintx(iInfo, True)
@@ -32,14 +32,28 @@ def getdetails(iInfo=None):
         instanceData = ec2R.Instance(iInfo[i]['id'])
         iInfo[i]['state'] = instanceData.state['Name']
         iInfo[i]['ami'] = instanceData.image_id
-        instanceTag = instanceData.tags
-        for j in range(len(instanceTag)):
-            if instanceTag[j]['Key'] == 'Name':
-                iInfo[i]['name'] = instanceTag[j]['Value']
-                break
+        iInfo[i]['name'] = gettagvalue(iInfo[i]['id'])
+        # instanceTag = instanceData.tags
+        # for j in range(len(instanceTag)):
+        #     if instanceTag[j]['Key'] == 'Name':
+        #         iInfo[i]['name'] = instanceTag[j]['Value']
+        #         break
     debg.dprintx("Details except AMI-name")
     debg.dprintx(iInfo, True)
     return (iInfo)
+
+
+def gettagvalue(instID, Tag="Name"):
+    instanceData = ec2R.Instance(instID)
+    instanceTag = instanceData.tags
+    if len(instanceTag) > 0:
+        for j in range(len(instanceTag)):
+            if instanceTag[j]['Key'] == Tag:
+                tagvalue = instanceTag[j]['Value']
+                break
+    else:
+        tagvalue = ""
+    return tagvalue
 
 
 def getaminame(instanceImgID):
@@ -47,16 +61,16 @@ def getaminame(instanceImgID):
     return (aminame)
 
 
-def getsshinfo(tarID):
-    tarInstance = ec2R.Instance(tarID)
+def getsshinfo(instID):
+    tarInstance = ec2R.Instance(instID)
     instanceIP = tarInstance.public_ip_address
     instanceKey = tarInstance.key_name
     instanceImgID = tarInstance.image_id
     return (instanceIP, instanceKey, instanceImgID)
 
 
-def startstop(tarID, cmdtodo):
-    tarInstance = ec2R.Instance(tarID)
+def startstop(instID, cmdtodo):
+    tarInstance = ec2R.Instance(instID)
     thecmd = getattr(tarInstance, cmdtodo)
     response = thecmd()
     return (response)
