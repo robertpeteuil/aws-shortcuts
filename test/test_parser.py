ab607715b3cb6f246730c3bb975f8254dde69c22
Robert Peteuil <robert@peteuil.com>
2017-04-26 09:23:34 -0700

    test - added help checking to test_parser

--- test/test_parser.py
+++ test/test_parser.py
@@ -7,11 +7,6 @@ from awss import setupParser
 parser = setupParser()
 
 
-def awsparse(args):
-    options = parser.parse_args(args)
-    return options
-
-
 @pytest.fixture(params=["start ", "stop "])
 def cmdname(request):
     return request.param
@@ -47,11 +42,20 @@ def username(request):
     return request.param
 
 
+@pytest.fixture(params=[("", "-h"), ("list ", "-h"), ("start ", "-h"),
+                        ("stop ", "-h"), ("ssh ", "-h")])
+def inhelp(request):
+    return request.param
+
+
 def test_list_parse_valid(inname, innum, instate, debugstate):
     print("test - list_parse_valid for:   name: %s, id: %s, state: %s,"
           " debug: %s" % (inname[0], innum[0], instate[0], debugstate[0]))
+
     args = "list " + inname[0] + innum[0] + instate[0] + debugstate[0]
-    options = awsparse(args.split())
+    parser = setupParser()
+    options = parser.parse_args(args.split())
+
     assert options.command == 'list'
     assert options.debug == debugstate[1]
     assert options.instname == inname[1]
@@ -62,8 +66,11 @@ def test_list_parse_valid(inname, innum, instate, debugstate):
 def test_startstop_parse_valid(cmdname, inname, innum, debugstate):
     print("test - start/stop_parse_valid for:   command: %s, name: %s, id: %s,"
           " debug: %s" % (cmdname, inname[0], innum[0], debugstate[0]))
+
     args = cmdname + inname[0] + innum[0] + debugstate[0]
-    options = awsparse(args.split())
+    parser = setupParser()
+    options = parser.parse_args(args.split())
+
     assert options.command == cmdname[:-1]
     assert options.debug == debugstate[1]
     assert options.instname == inname[1]
@@ -73,9 +80,22 @@ def test_startstop_parse_valid(cmdname, inname, innum, debugstate):
 def test_ssh_parse_valid(inname, innum, debugstate):
     print("test - ssh_parse_valid for:   name: %s, id: %s, debug: %s" %
           (inname[0], innum[0], debugstate[0]))
+
     args = "ssh " + inname[0] + innum[0] + debugstate[0]
-    options = awsparse(args.split())
+    parser = setupParser()
+    options = parser.parse_args(args.split())
+
     assert options.command == "ssh"
     assert options.debug == debugstate[1]
     assert options.instname == inname[1]
     assert options.id == innum[1]
+
+
+def test_list_parse_help(inhelp):
+    print("test - list_parse_help for:   command: %s" % (inhelp[0]))
+
+    args = inhelp[0] + inhelp[1]
+    parser = setupParser()
+    with pytest.raises(SystemExit):
+        options = parser.parse_args(args.split())
+        pass
