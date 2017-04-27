e3002e78e265868d84f2c9775a0709ef0cc5534d
Robert Peteuil <robert@peteuil.com>
2017-04-26 17:20:30 -0700

    Test - updated picklist with new case to test abort option

--- test/test_picklist.py
+++ test/test_picklist.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from builtins import range
 import pytest
 import mock
 
-from awss.colors import CLRnormal, CLRheading, CLRtitle, CLRwarning, \
-    CLRerror, statCLR
-import awss.awsc as awsc
+from awss import det_instance
 import awss.debg as debg
-from awss import det_instance, user_picklist, list_instances, user_entry
 
-# flake8: noqa
-
-debg.init()  # turn off debug-print messages
+debg.init(False, False)
 
 
-@pytest.mark.parametrize(("ids", "kys", "anames", "ilist", "ide", "idx"), [
+@pytest.mark.parametrize(("ids", "kys", "anames", "ilist", "ide"), [
     ({0: {'id': 'i-0c875fafa1e71327b'},
      1: {'id': 'i-03120c2544fdf5b6f'},
      2: {'id': 'i-0014d16e7f68ce746'},
@@ -38,15 +32,24 @@ debg.init()  # turn off debug-print messages
           'state': 'running'},
       4: {'ami': 'ami-2cade64c', 'id': 'i-0341963e139617c75', 'name': 'RHEL',
           'state': 'running'}},
-     'i-0014d16e7f68ce746', 2),
+     'i-0014d16e7f68ce746'),
     ({0: {'id': 'i-0341963e139617c75'}},
      ['1'],
      {'ami-2cade64c': 'RHEL-7.3_HVM_GA-20161026-x86_64-1-Hourly2-GP2'},
      {0: {'ami': 'ami-2cade64c', 'id': 'i-0341963e139617c75', 'name': 'RHEL',
           'state': 'running'}},
-     'i-0341963e139617c75', 0),
-    ({}, ['1'], {}, {}, '0', 0)])
-def test_det_target(ids, kys, anames, ilist, ide, idx):
+     'i-0341963e139617c75'),
+    ({0: {'id': 'i-0c875fafa1e71327b'},
+      1: {'id': 'i-03120c2544fdf5b6f'}},
+     ['a', '7', '0'],
+     {'ami-16efb076': 'ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64',
+      'ami-165a0876': 'amzn-ami-hvm-2016.09.1.20170119-x86_64-gp2'},
+     {0: {'ami': 'ami-16efb076', 'id': 'i-0c875fafa1e71327b', 'name': 'Ubuntu',
+          'state': 'running'},
+      1: {'ami': 'ami-165a0876', 'id': 'i-03120c2544fdf5b6f', 'name': 'Amazon',
+          'state': 'running'}}, 0),
+    ({}, ['1'], {}, {}, 0)])
+def test_det_target(ids, kys, anames, ilist, ide):
     global counter
     counter = 0
 
@@ -72,13 +75,12 @@ def test_det_target(ids, kys, anames, ilist, ide, idx):
     with mock.patch('awss.awsc.getdetails', getlocaldetails, create=True):
         with mock.patch('awss.awsc.getaminame', getlocalaminame, create=True):
             with mock.patch('awss.getchar._Getch.int', RetKey, create=True):
-                if ide != '0':
-                    (tarID, tarIndex) = det_instance("ssh", ids,
-                                                     "TEST")
-                    assert tarID == ide
-                    assert tarIndex == idx
+                if ide:
+                    debg.init(True, True)
+                    tar_inst = det_instance("ssh", ids, "TEST")
+                    assert tar_inst == ide
                 else:
                     with pytest.raises(SystemExit):
-                        (tarID, tarIndex) = det_instance("ssh", ids,
-                                                         "TEST")
+                        debg.init(True, True)
+                        tar_inst = det_instance("ssh", ids, "TEST")
                         pass
